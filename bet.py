4c4c0f30400541ba68b6fce22af841e35e25143d
IRLadderGoat <bumbi@live.dk>
2020-11-14 15:03:10 +0100

    now able to add matches to betslip

--- bet.py
+++ bet.py
@@ -1,16 +1,36 @@
-#import database as db
-#import scrape
+import database as db
+import scrape
 #from bs4 import BeautifulSoup as bs4
 from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 import chromedriver_autoinstaller
+import time
 
 chromedriver_autoinstaller.install()
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path="/home/matthew/Documents/git-repos/csgo-nn/chromedriver")
 driver.get("https://loot2.bet/sport/esports/counter-strike")
 
 def AddMatchesToBetSlip():
-    pass
+    time.sleep(2)
+    PREDICTION_MIN_COEF = 60
+    MATCH_MIN_ODDS = 1.8
+    bettable_matches = driver.find_elements_by_tag_name("app-market")
+    matches_db = db.get_not_bet_matches()
+    for match in bettable_matches:
+        team_b = match.find_element_by_class_name("teamRight")
+        team_a = match.find_element_by_class_name("teamLeft")
+        match_coefs_elements = match.find_elements_by_class_name("cof")
+        match_coefs = (float(match_coefs_elements[0].text), float(match_coefs_elements[1].text))
+        a_name = scrape.clean_name(team_a.text)
+        b_name = scrape.clean_name(team_b.text)
+        for match_db in matches_db:
+            if match_db['team_a'] == a_name and match_db['team_b'] == b_name:
+                coef = match_db['percentage_a_b'].split("/") 
+                if int(coef[0]) > PREDICTION_MIN_COEF and match_coefs[0] >= MATCH_MIN_ODDS:
+                  match_coefs_elements[0].click()
+                elif int(coef[1]) > PREDICTION_MIN_COEF and match_coefs[1] >= MATCH_MIN_ODDS:
+                  match_coefs_elements[1].click()
+        
 
 def Login():
     login_button = driver.find_element_by_xpath("//*[@id='main']/app-header/header/nav/div/div/a[3]")
@@ -48,7 +68,8 @@ def GetBettable():
         
 def main():
     #Login()
-    GetBettable()
+    #GetBettable()
+    AddMatchesToBetSlip()
     pass
 
 
